ac7bdd55e9d1a52e1a6eac36db15ec95c39f1502
Supriya Kumari <supriya17feb99@gmail.com>
2020-08-10 11:42:38 +0530

    Recursion

---
+++ Recursion.java
@@ -0,0 +1,28 @@
+package sets;
+import java.util.*;
+
+public class Recursion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		System.out.println(sum(n));
+	
+
+	}
+	public static int sum(int n)
+	{
+		if(n==1)
+			return 1;
+		else
+		{  
+			return n+sum(n-1);
+		  
+		}
+	}
+
+}
