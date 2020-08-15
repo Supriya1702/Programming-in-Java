3b2ba6ae8fe53bd6c608ff3f0f871e87bd7b393b
Supriya <supriya17feb99@gmail.com>
2020-08-16 02:49:26 +0530

    chnaged class from Codevita2 to SwapByRef

--- SwapByRef.java
+++ SwapByRef.java
@@ -2,19 +2,19 @@ package Problems;
 
 import java.util.Scanner;
 
-public class Codevita2 {
+public class SwapByRef {
 	
 	
 int a ,b;
 
 
-Codevita2(int a , int b)
+SwapByRef(int a , int b)
 {
 	this.a = a;
 	this.b=b;
 }
 
-void swap( Codevita2 stn)
+void swap( SwapByRef stn)
 {
 	int temp ;
 	temp=stn.a;
@@ -26,7 +26,7 @@ void swap( Codevita2 stn)
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Codevita2 c = new Codevita2(10,20);
+		SwapByRef c = new SwapByRef(10,20);
 //		c.a=12;
 //		c.b=10;
 		System.out.println(c.a + " "+ c.b);
