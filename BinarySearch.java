8f2eedf16747401385c37ab8a35b801d329599d1
Supriya1702 <38047163+Supriya1702@users.noreply.github.com>
2020-08-19 15:27:47 +0530

    Update BinarySearch

--- BinarySearch.java
+++ BinarySearch.java
@@ -31,5 +31,28 @@ public class BinarySearch {
 	
 		
 	}
+	
+	
+	static int bsint(int a[] , int key , int low , int high)
+	{
+	
+		int mid = (low +high)/2;
+		
+		if(a[mid] == key)
+			return mid+1;
+		
+		else if( key <a[mid])
+			 bsint(a,key,low , mid-1);
+		
+		else if ( key > a[mid])
+			return bsint(a,key,mid+1,high);
+		
+		//for the sake of returning int value by method
+		//Also , return bsint(...) is necessry when recursively calling 
+		//or else it returns -1 instead ...the ultimate return of method;
+		return -1;
+		
+	}
+
 
 }
