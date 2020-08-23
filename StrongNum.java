77d71b60a455ad4e89cd9dfd908b8e3503bb1f48
Supriya <supriya17feb99@gmail.com>
2020-08-23 15:31:01 +0530

    Strong Number definition added

--- StrongNum.java
+++ StrongNum.java
@@ -4,13 +4,15 @@ import java.util.*;
 public class StrongNum {
 
 	public static void main(String[] args) {
+
+	 // Strong Nummber is a number that is equal to the sum of the factorial of its digits
 		
      Scanner in = new Scanner(System.in);
      
      int num = in.nextInt();
 	 
 	 // for calculating the number of digits 
-	 
+
     int  numDigits= String.valueOf(num).length();
     
      int temp = num  , sum = 0 , digit = Integer.MIN_VALUE  , fact = Integer.MIN_VALUE;
