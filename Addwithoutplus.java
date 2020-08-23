2b3d12274b6e079d4749f71673d6a4d66ef9688b
Supriya1702 <38047163+Supriya1702@users.noreply.github.com>
2020-08-23 17:54:06 +0530

    for negative values

--- Addwithoutplus.java
+++ Addwithoutplus.java
@@ -5,19 +5,35 @@ public class Addwithoutplus {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a = 5 , b =3;
+		// Jisme add hora hai wo sum hai
 		
+		int a = -5 , b =3;
+		
+		if(a>0)
+		{
 		while( a!=0)
 		{
 			a--;
 			b++;
 		}
+		System.out.println("Sume is" +b);
+		}
 		
-		System.out.println("Sum is " + b);
+		else if ( a< 0)
+		{
+			while(b!=0)
+			{
+				a++;
+				b--;
+			}
+		}
 		
+		System.out.println("Sum is " + a);
+		
+		}
 	}
 	
-	
+	 // For postive
 	/*  a => 1+ 1 +1+ 1+ 1
 	 *  b => 1 + 1+ 1
 	 * 
@@ -26,7 +42,13 @@ public class Addwithoutplus {
 	 * 
 	 *  when a = 0
 	 *  b=> 1+ 1+ 1....a number of times
+	 *  
+	 *  for negative a 
+	 *  a++ and b--
 	 */
 	
+	
+	
+	
 
-}
+
