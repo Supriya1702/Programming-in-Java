ff3f210422350dfb9aeeb89f61023e2a6b508a50
Supriya <supriya17feb99@gmail.com>
2020-09-16 22:34:05 +0530

    array rotation added

---
+++ ArrayRotation.java
@@ -0,0 +1,57 @@
+package Problems;
+import java.util.*;
+public class ArrayRotation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter the size of the array");
+		int n = in.nextInt() ;
+		
+		//int arr[] = {1,2,3,4,5};
+		int arr[] = new int[n] ;
+		
+		for( int i = 0 ; i< arr.length ; i++)
+		          arr[i] = in.nextInt() ;
+		
+		int len = arr.length ;
+		
+		int d = 2 ; // rotation by the number of elements
+		
+		int arr2[] = new int[len + d] ;
+		
+		for( int i = 0 ; i < len ; i++ )
+		{
+			
+			arr2[i+2] = arr[i] ;
+			
+		}
+		
+		int len2 = arr2.length ;
+//		arr2[0] = arr2[len2-2] ;
+//		arr2[1] = arr2[len2-1] ;
+//		
+//		arr2[len2-1]=0;
+//		arr2[len2-2]=0;
+		
+		
+		int j = 0 ;
+		
+		while( j < d)
+		{
+			
+			arr2[j] = arr2[ len2 - d + j];
+			j++ ;
+		}
+//		
+		for( int i = 0 ; i < arr2.length - d ; i++)
+			System.out.println(arr2[i]);
+			
+		
+		
+	}
+
+}
