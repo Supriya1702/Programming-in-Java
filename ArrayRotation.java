3798010051616c2dbd6102b4bc8c056d73ab5b90
Supriya1702 <38047163+Supriya1702@users.noreply.github.com>
2020-09-16 22:40:18 +0530

    Update ArrayRotation.java

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -5,7 +5,7 @@ public class ArrayRotation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		//RIGHT ARRAY ROTATION
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Enter the size of the array");
