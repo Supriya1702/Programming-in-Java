d2394aa4a1e5e639938272fe001bbe81e91b2c26
Supriya <supriya17feb99@gmail.com>
2020-10-04 19:41:53 +0530

    Added O(n) solution for Sum of pair of integers

--- SumPair.java
+++ SumPair.java
@@ -1,5 +1,5 @@
 package Problems;
-
+import java.util.*;
 public class SumPair {
 
 	public static void main(String[] args) {
@@ -8,7 +8,10 @@ public class SumPair {
 		int arr[] = { 1,2,3,5,6,7,10};
 		
 		int n = 7;
+		// This method finds the pair of integers having the sum n in O(n2)
 		sum(arr,n);
+		//this method reuires O(n) of space and O(n) of time complexity
+		sumInN(arr,n);
 
 	}
 	
@@ -22,6 +25,27 @@ public class SumPair {
 				{if(arr[i] + arr[j] == n)
 					System.out.println(arr[i]+"\t"+arr[j]);
 				}
+		}
+	}
+	
+	
+	static void sumInN(int[] arr , int n)
+	{
+		int num=0;
+		HashSet<Integer> set = new HashSet(Arrays.asList(arr));
+		
+		for(int i = 0 ; i < arr.length ; i++)
+		{
+			set.add(arr[i]);
+			
+		}
+
+		for(int i = 0 ; i < arr.length ; i++)
+		{
+			
+			num =n- arr[i];
+		    if(set.contains(num))
+		    	System.out.println(arr[i]+"\t"+num);
 		}
 	}
 
